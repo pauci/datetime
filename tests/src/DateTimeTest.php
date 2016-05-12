d8efabcbfd23a3ef0cfb8c5e6b93b23986e1dd1c
pauci <pauci.sk@gmail.com>
2016-05-12 23:16:52 +0200

    Comparison test

--- tests/src/DateTimeTest.php
+++ tests/src/DateTimeTest.php
@@ -16,6 +16,27 @@ class DateTimeTest extends \PHPUnit_Framework_TestCase
 
     }
 
+    public function testComparison()
+    {
+        $dateTime1 = DateTime::fromString('2016-05-12 22:37:46');
+        $dateTime2 = DateTime::fromString('2016-05-12 22:37:46.000000');
+
+        self::assertTrue($dateTime1 == $dateTime2);
+        self::assertFalse($dateTime1 != $dateTime2);
+
+        $dateTime3 = DateTime::fromString('2017-01-10 12:20:11');
+        $dateTime4 = DateTime::fromString('2015-02-22 14:33:54');
+
+        self::assertTrue($dateTime3 > $dateTime4);
+        self::assertFalse($dateTime3 <= $dateTime4);
+
+        $dateTime5 = DateTime::fromString('2016-05-12 22:37:46.000001');
+        $dateTime6 = DateTime::fromString('2016-05-12 22:37:46');
+
+        self::assertTrue($dateTime5 > $dateTime6);
+        self::assertFalse($dateTime5 <= $dateTime6);
+    }
+
     public function testToString()
     {
         $dateTime = DateTime::fromString('2016-05-12 22:37:46');
