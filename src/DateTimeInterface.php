bfb55146c60f95a9a7bf77ad453607052423ba5a
Pavol Kirschbaum <pkirschbaum@sygic.com>
2016-09-22 13:50:27 +0200

    Add new modifier method inDefaultTimezone()

--- src/DateTimeInterface.php
+++ src/DateTimeInterface.php
@@ -5,6 +5,11 @@ namespace Pauci\DateTime;
 interface DateTimeInterface extends \DateTimeInterface, \JsonSerializable
 {
     /**
+     * @return DateTimeInterface
+     */
+    public function inDefaultTimezone();
+
+    /**
      * @return string
      */
     public function toString();
